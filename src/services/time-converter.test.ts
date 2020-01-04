060ca51b076b5d0fa6d08bc7631f37026ba01d55
bryant8888 <javacenter2013@gmail.com>
2020-01-04 15:13:52 +0300

    ARGENT-02 Finished Service with test

--- src/services/time-converter.test.ts
+++ src/services/time-converter.test.ts
@@ -15,8 +15,7 @@ describe('TimeConverter', () => {
         clientTime = new Time(new Date("2017-01-26"));
     });
 
-
-    // to backendTime
+    // Converter to backendTime
     it('should transform input object to another object', () => {
         const rslt: Time = timeConverter.straight(backendTime);
 
@@ -72,7 +71,7 @@ describe('TimeConverter', () => {
 
         invalidBackendTimeValues.forEach((value: BackendTime): void => {
             const testWithError = (): void => {
-                timeConverter.straight(value)
+                timeConverter.straight(value);
             };
 
             expect(testWithError).toThrow(Error);
@@ -84,11 +83,9 @@ describe('TimeConverter', () => {
 
             expect(testWithoutError).not.toThrow(Error);
         });
-
-
     });
 
-    // to clientTime
+    // Converter to clientTime
     it('should reverse transformation input object to another object', () => {
         const rslt: BackendTime = timeConverter.reverse(clientTime);
 
@@ -99,7 +96,6 @@ describe('TimeConverter', () => {
 
     it('should fail if date is invalid', () => {
         const invalidDate: Time = new Time(new Date(1000000000000000000000000000000000));
-
         const test = () => {
             timeConverter.reverse(invalidDate);
         };
@@ -107,15 +103,31 @@ describe('TimeConverter', () => {
         expect(test).toThrow(Error);
     });
 
-    it('should correct transform values', () => {
-        const rslt: Time = timeConverter.straight(backendTime);
+    it('should correct transform to Time', () => {
+        const rslt: BackendTime = timeConverter.reverse(clientTime);
 
-        expect(rslt.date.getFullYear()).toBe(backendTime.year);
-        expect(rslt.date.getDate()).toBe(backendTime.day);
-        expect(rslt.date.getMonth()).toBe(backendTime.month + 1);
+        expect(rslt.day).toBe(clientTime.date.getDate());
+        expect(rslt.month).toBe(clientTime.date.getMonth() + 1);
+        expect(rslt.year).toBe(clientTime.date.getFullYear());
     });
 
     it('should have same values after appl straight + reverse methods to initial value', () => {
+        const srcTime: Time = new Time(new Date("2017-01-26"));
+        const transformedTime: Time = timeConverter.straight(timeConverter.reverse(srcTime));
+        const srcBackendTime: BackendTime = {
+            year: 2000,
+            day: 1,
+            month: 2
+        };
+        const transformedBackendTime: BackendTime = timeConverter.reverse(timeConverter.straight(srcBackendTime));
 
+
+        expect(srcTime.date.getDate()).toBe(transformedTime.date.getDate());
+        expect(srcTime.date.getMonth()).toBe(transformedTime.date.getMonth());
+        expect(srcTime.date.getFullYear()).toBe(transformedTime.date.getFullYear());
+
+        expect(srcBackendTime.day).toBe(transformedBackendTime.day);
+        expect(srcBackendTime.month).toBe(transformedBackendTime.month);
+        expect(srcBackendTime.year).toBe(transformedBackendTime.year);
     });
 });
