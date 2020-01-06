3c1b6e2abad473ce2ab9a71468f64cc2c1c75f1f
Rehten <Mroow@yandex.ru>
2020-01-06 17:45:22 +0300

    ARGENT-05 - hotfix

--- src/components/argent-input/index.tsx
+++ src/components/argent-input/index.tsx
@@ -1,8 +1,8 @@
-import {Form} from "react-bootstrap";
+import {Form, FormControlProps} from "react-bootstrap";
 import React from "react";
 
-interface Props {
+interface Props extends FormControlProps {
     dataTest?: string;
 }
 
-export const ArgentInput = (props: Props) => (<div data-test={props.dataTest}><Form.Control /></div>);
+export const ArgentInput = (props: Props) => (<div data-test={props.dataTest}><Form.Control {...props} /></div>);
