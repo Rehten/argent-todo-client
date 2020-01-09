89e84b7deec9f6b2c08663fda287f63d4556256d
bryant8888 <javacenter2013@gmail.com>
2020-01-09 20:24:01 +0300

    ARGENT-06 Create button wrapper components with test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,58 @@
-import React from 'react';
-import {ArgentButton} from "./components/argent-button";
+import React, { useState } from 'react';
+import { Form } from "react-bootstrap";
+import { ArgentInput } from "./components/argent-input";
 
-const App: React.FC = () => <ArgentButton variant="secondary">123</ArgentButton>;
+const App: React.FC = () => {
+    const [name, setName] = useState('');
+    console.log(name);
+
+    return (
+        <div className="container">
+            <h2 style={{marginTop: '2rem'}}>OnChange Input</h2>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Your Name</Form.Label>
+                <ArgentInput
+                    type="text"
+                    // placeholder="Enter your name"
+                    value={name}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setName(event.target.value)}
+                />
+            </Form.Group>
+        </div>
+    )
+};
+
+// class App extends React.Component {
+//     state = {
+//         name: ''
+//     };
+//
+//     handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+//         this.setState({
+//             name: event.target.value
+//         })
+//     };
+//
+//
+//
+//     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+//         console.log(this.state);
+//         return (
+//             <div className="container">
+//                 <h2 style={{marginTop: '2rem'}}>OnChange Input</h2>
+//                 <Form.Group controlId="formBasicEmail">
+//                     <Form.Label>Your Name</Form.Label>
+//                     <ArgentInput
+//                         type="text"
+//                         // placeholder="Enter your name"
+//                         // value={name}
+//                         onChange={this.handleChangeInput}
+//                     />
+//                 </Form.Group>
+//             </div>
+//         )
+//     }
+//
+// }
 
 export default App;
--- src/components/argent-button/index.tsx
+++ src/components/argent-button/index.tsx
@@ -4,14 +4,14 @@ import { Button, ButtonProps } from "react-bootstrap";
 
 interface Props extends ButtonProps {
     dataTest?: string;
-    children?: string;
+    children: string;
     onClick?: () => void;
 }
 
 export const ArgentButton = ({dataTest, onClick, ...otherProps}: Props) => {
     return (
         <div data-test={dataTest}>
-            <Button {...otherProps} onClick={onClick} />
+            <Button {...otherProps} onClick={onClick}>{otherProps.children}</Button>
         </div>
     );
 };
